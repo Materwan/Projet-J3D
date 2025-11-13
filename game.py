0efab2f4ae5be9e85314bb7d4d7ae50927488795
Materwan <erwan.mercier972@gmail.com>
2025-11-13 18:57:20 +0100

    Creating player class and basic actions

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 screen = pygame.display.set_mode((700, 700))
 
@@ -17,33 +18,24 @@ class Game:
         self.frame = 0
         self.i = 0
         self.position = [0, 0]
+        self.player = Player(self.screen)
         self.clock = pygame.time.Clock()
 
     def event(self):
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 self.running = False
-            
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.pressed = True
-                    self.frame = 1
-            
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.pressed = False
 
-        if self.pressed:
-            self.i = self.frame // 6 % 3
+        self.player.event(events)
 
     def update(self):
-        self.frame = self.frame + 1 if self.frame < 59 else 0
-        if self.pressed:
-            self.position[0] += 2
+        self.player.update()
 
     def display(self):
         self.screen.fill((0, 0, 0))
-        self.screen.blit(animation[self.i], self.position)
+        self.player.display()
+        #self.screen.blit(animation[self.i], self.position)
         pygame.display.update( )
 
     def run(self):
--- player.py
+++ player.py
@@ -1,2 +1,59 @@
 import pygame
 
+screen = pygame.display.set_mode((0, 0))
+
+ANIMATIONS = {"right": ["runAnimation/run animation frame 1.png",
+                        "runAnimation/run animation frame 2.png",
+                        "runAnimation/run animation frame 3.png"],
+              "left": ["runAnimation/run animation frame 1.png",
+                       "runAnimation/run animation frame 2.png",
+                       "runAnimation/run animation frame 3.png"]}
+flip = False
+for x in ANIMATIONS.keys():
+    ANIMATIONS[x] = list(map(lambda i: pygame.transform.flip(pygame.transform.scale(pygame.image.load(i).convert_alpha(), (100, 100)), flip_x=flip, flip_y=False), ANIMATIONS[x]))
+    flip = True
+
+class Player:
+
+    def __init__(self, screen: pygame.surface.Surface):
+        self.screen = screen
+        self.position = [0, 0]
+        self.speed = [0, 0]
+        self.velocity =  [0, 0]
+        self.direction = "right"
+        self.pressed = False
+    
+    def event(self, events: pygame.event.Event):
+        
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.direction = "right"
+                    self.pressed = True
+                    self.velocity[0] = 2
+                if event.key == pygame.K_LEFT:
+                    self.direction = "left"
+                    self.pressed = True
+                    self.velocity[0] = -2
+            
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.pressed = False
+                    self.velocity[0] = 0
+                if event.key == pygame.K_LEFT:
+                    self.pressed = False
+                    self.velocity[0] = 0
+    
+    def update(self):
+        #print(self.pressed)
+        if self.pressed:
+            self.speed[0] += self.velocity[0]
+            self.speed[1] += self.velocity[1]
+
+        self.position[0] += self.speed[0]
+        self.position[1] += self.speed[1]
+
+        self.speed = [0, 0]
+
+    def display(self):
+        self.screen.blit(ANIMATIONS[self.direction][0], self.position)
