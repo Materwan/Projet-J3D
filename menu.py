2266a8950b4f3f7d893b43f0b3e4ac489a44b07c
Titouan111222 <titouan.marty@epita.fr>
2025-11-15 00:45:11 +0100

    class Text

--- menu.py
+++ menu.py
@@ -2,13 +2,25 @@ import pygame as p
 
 p.init()
 
+p.font.init()
+my_font = p.font.SysFont('Comic Sans MS', 30)
+
 WINDOWS = p.display.get_desktop_sizes()[0]   #fullscreen
 
 screen = p.display.set_mode(WINDOWS)
 
+
+class Text:
+
+    def __init__(self,name,size,text,t_color,antialias=False):
+        self.text = p.font.SysFont(name,size).render(text,antialias,t_color)
+    
+    def draw_text(self,coordonninate):
+        screen.blit(self.text,coordonninate)
+
 class Bouton:
 
-    def __init__(self, left, top, width, height, color):
+    def __init__(self,left,top,width,height,color,text=Text('',0,'',(0,0,0))):
         self.left = left
         self.top = top
         self.width = width
@@ -16,11 +28,13 @@ class Bouton:
         self.color = p.Color(color)
         self.rec = p.Rect(left, top, width, height)
         self.surface = p.Surface((width, height))
+        self.text = text
 
 
     def draw(self):                     #affiche le bouton
         self.surface.fill(self.color)
         screen.blit(self.surface, (self.left, self.top))
+        self.text.draw_text((self.left+(self.width//4),self.top+(self.height//3)+10))
 
 class Menu:
 
@@ -28,7 +42,7 @@ class Menu:
         self.screen = screen
         self.clock = p.time.Clock()
         self.running = True
-        self.bouton = Bouton(WINDOWS[0]-200,0,200,200,(0, 0, 255))
+        self.bouton = Bouton(WINDOWS[0]-200,0,200,200,(0, 0, 255),Text('Comic Sans MS',30,'Quitter',(255,0,0)))
 
     def event(self):
         events = p.event.get()
@@ -44,7 +58,7 @@ class Menu:
         p.display.update()
 
     def display(self):
-        screen.fill((0, 0, 0)) #background
+        self.screen.fill((0, 0, 0)) #background
         self.bouton.draw()
 
     def run(self):
