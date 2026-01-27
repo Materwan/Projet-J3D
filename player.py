799fc91913c0bb1b116e00b78b1ea40703d5e11c
Thibaut Sancier <thibaut.sancier@epita.fr>
2026-01-27 14:10:00 +0100

    opti main.py / et réseau

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from moteur import Moteur
+from typing import Tuple
 
 def load_image(path: str, size: tuple[int, int]) -> pygame.surface.Surface:
 
@@ -70,15 +71,20 @@ class Player:
         self.screen = screen
         self.moteur = Moteur(self.screen)
         self.Animation = Animation(self.screen)
-        self.keybinds = None # il sera defini quand on passe du menu au game
+        self.keybinds = {
+            "up": pygame.K_UP,
+            "down": pygame.K_DOWN,
+            "left": pygame.K_LEFT,
+            "right": pygame.K_RIGHT,
+        } # il sera defini quand on passe du menu au game
     
         self.hitbox = pygame.Rect(x, y, 32, 15)
-        self.velocity = pygame.math.Vector2(0, 0) # == [0, 0]
+        self.velocity = pygame.Vector2(0, 0) # == [0, 0]
         self.direction = "right"
         self.is_moving = False
 
 
-    def event(self, keys):
+    def event(self, keys: Tuple[bool]):
         # gestion de la vélocité en regardant les touches pressées
         self.velocity.update(
             keys[self.keybinds["right"]] - keys[self.keybinds["left"]],
@@ -87,18 +93,17 @@ class Player:
         # vaut soit -1, 0 ou 1 pour la velocité en x et en y 
 
     def update(self):
-        is_moving = self.velocity.length_squared() > 0 # si != [0, 0]
-        if is_moving: # si le joueur se deplace alors :
+        moving_intent = self.velocity.length_squared() > 0
+        if moving_intent: # si le joueur veut se deplacer alors :
 
             # gestion collision
-            nearby_obstacles = self.moteur.get_nearby_obstacles(self.hitbox)
-            self.moteur.collision(self.hitbox, self.velocity, nearby_obstacles)
+            self.moteur.collision(self.hitbox, self.velocity)
 
-            is_moving = self.velocity.length_squared() > 0
-            if is_moving: # si on se deplace toujours alors :
+            is_moving = self.velocity.length_squared()
+            if is_moving > 0: # si on se deplace toujours alors :
                 
                 # gestion des déplacements en diagonales
-                if self.velocity.length_squared() > 1: # si deplacement en diagonale
+                if is_moving > 1: # si deplacement en diagonale
                     self.velocity.normalize_ip() # [1, 1] devient [0.707107, 0.707107]
             
                 # gestion de la position de la hitbox / modifie la position de la hitbox en décalant de x et y
@@ -110,7 +115,7 @@ class Player:
                 elif self.velocity.x > 0:
                     self.direction = "right"
 
-        self.Animation.update(self.direction, is_moving)
+        self.Animation.update(self.direction, moving_intent)
 
     def display(self):
         self.Animation.display((self.hitbox.x - 34, self.hitbox.y - 70))
@@ -129,36 +134,29 @@ class Hôte:
         # dico des infos des joueurs : hitbox, velocité, direction, config de touche, ext..
 
     def event(self, keys):
-        # gestion de la vélocité en regardant les touches pressées
-        self.Lplayer["player1"][1][0] = int(keys[self.keybinds["right"]]) - int(
-            keys[self.keybinds["left"]]
-        )  # vaut soit 0, 1 ou -1 pour la vélocité en x
-        self.Lplayer["player1"][1][1] = int(keys[self.keybinds["down"]]) - int(
-            keys[self.keybinds["up"]]
-        )  # vaut soit 0, 1 ou -1 pour la vélocité en y
-
-    def receive_info(self):
         """
-        recupere des infos : (nom, keybinds, touche appuyé) des joueurs 
+        Modifie la vélocité en regardant les touches pressées
+        x et y vale chacun soit -1, 0 ou 1
         """
-        pass
+        self.velocity.update(
+            keys[self.keybinds["right"]] - keys[self.keybinds["left"]],
+            keys[self.keybinds["down"]] - keys[self.keybinds["up"]]
+        )
 
     def update(self):
         """
-        traite les infos donc gestion de la velocité, hitbox, direction, collision pour tous les joueurs
-        """
-        pass
-
-    def give_info(self):
-        """
-        donne des infos a tous les joueurs : (la carte (ou seed) une fois) 
-        et (position, direction, bool) des joueurs  
+        - doit recuperer : la vélocité de tous les joueurs
+        - traite l'info : verification velocité
+        - gestion collision, position et direction de tous les joueurs
+        - gestion de l'animation de chaque joueur (frame)
+        - doit envoyer des infos : position, direction 
+          et is_moving de tous les joueurs (une fois la map ou seed)
         """
         pass
 
     def display(self):
         """
-        affiche tous les joueurs grâce à leurs positions, directions et bool (bool = en mouvement)
+        affiche tous les joueurs grâce à leurs positions, directions et is_moving
         """
         pass
 
@@ -181,30 +179,28 @@ class Client:
         self.Animation= Animation(self.screen)
         self.keybinds = None
     
-    def event(self):
-        pass
-
-    def give_info(self):
+    def event(self, keys):
         """
-        donne des infos : nom, keybinds, touche appuyé, ...
+        Modifie la vélocité en regardant les touches pressées
+        x et y vale chacun soit -1, 0 ou 1
         """
-        pass
-
-    def receive_info(self):
-        """
-        recupere une info unique : la carte (ou seed)
-        recupere des infos : (position, direction, bool) des joueurs  
-        """
-        pass
+        self.velocity.update(
+            keys[self.keybinds["right"]] - keys[self.keybinds["left"]],
+            keys[self.keybinds["down"]] - keys[self.keybinds["up"]]
+        )
 
     def update(self):
         """
-        gere au moins l'animation de chaque joueur
+        - doit donner des infos : vélocité, ...
+        - gestion de l'animation de chaque joueur (frame)
+        - doit recuperer des infos : position, direction 
+          et is_moving de tous les joueurs (une fois la map ou seed)
         """
         pass
 
     def display(self):
         """
-        affiche tous les joueurs grâce à leurs positions, directions et bool (bool = en mouvement)
+        affichage de tous les joueurs via Animation
+        grâce à leurs positions, directions et is_moving
         """
         pass
