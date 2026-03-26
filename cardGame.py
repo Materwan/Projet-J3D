63cafcf9e961ce309a41394c4b5b730bbea684dd
Clément Bellot <clement.bellot@epita.fr>
2026-03-26 15:42:07 +0100

    Tweaked card class, added visual changes based on Deck and Discards changes

--- cardGame.py
+++ cardGame.py
@@ -3,21 +3,27 @@ from typing import Tuple
 from menu import Text
 from random import shuffle
 
-CARD_TYPES = {0 : "Player", 1 : "Ally", 2 : "Spell", 3 : "Equip"}
+#Name : (Type, Artwork)
+CARD_LIST = {"Dmg2" : ("Spell", "Ressources/Cartes/PlaceHolderSpell.png"),
+             "HpUp3" : ("Equip", "Ressources/Cartes/PlaceHolderEquip.png")}
 
 class Card:
-    def __init__(self, type: int, name: str):
-        self.type = type
+    def __init__(self, name: str):
         self.name = name
+        if name in CARD_LIST:
+            self.type = CARD_LIST[name][0]
+            self.artwork = pygame.image.load(CARD_LIST[name][1])
+        else:
+            self.type = "None"
+            self.artwork = pygame.image.load("Ressources/Cartes/NoCard.png")
     
     def __str__(self):
-        return f"({CARD_TYPES[self.type]}) {self.name}"
+        return f"({self.type} {self.name})"
     
     def display(self):
         self.artwork = pygame.transform.scale(self.artwork, self.scale)
         self.screen.blit(self.artwork, self.position)
       
-
 class Hand:
     def __init__(self):
         self.cards = []
@@ -38,15 +44,19 @@ class Hand:
             printer += f"{str(self.cards[i])}, "
         printer += "]"
         return printer
-
+    
+    def display(self):
+        for cards in self.cards:
+            cards.display()
 
 class Deck:
-    def __init__(self, screen: pygame.surface.Surface, position: Tuple[int,int], cards: list[Card],  artwork = "Ressources/Cartes/PlaceHolderDeck.png"):
+    def __init__(self, screen: pygame.surface.Surface, position: Tuple[int,int], cards: list[Card],  artworkCard = "Ressources/Cartes/PlaceHolderDeck.png", artworkEmpty = "Ressources/Cartes/PlaceHolderDeckEmpty.png"):
         self.screen = screen
         self.position = (position)
         self.cards = cards
         self.size = len(self.cards)
-        self.artwork = pygame.image.load(artwork)
+        self.artworkCard = pygame.image.load(artworkCard)
+        self.artworkEmpty = pygame.image.load(artworkEmpty)
         self.scale = (35*5, 47*5)
           
     def add2Deck(self, cards: Card):
@@ -54,23 +64,26 @@ class Deck:
         self.size += 1
     
     def rmFromTopDeck(self):
-        self.size -=1
-        return self.cards.pop()
+        if(self.size != 0):
+            self.size -=1
+            return self.cards.pop()
     
     def shuffle(self):
         shuffle(self.cards)
 
     def __str__(self):
-        printer = "[ "
+        printer = "["
         for i in range(self.size):
             printer += f"{str(self.cards[i])}, "
-        printer += "]"
+        printer += f"{str(self.cards[-1])}]"
         return printer
     
     def display(self):
-        self.artwork = pygame.transform.scale(self.artwork, self.scale)
+        if (self.size == 0):
+            self.artwork = pygame.transform.scale(self.artworkEmpty, self.scale)
+        else:
+            self.artwork = pygame.transform.scale(self.artworkCard, self.scale)
         self.screen.blit(self.artwork, self.position)
-
 
 class DiscardPile:
     def __init__(self, screen: pygame.surface.Surface, position: Tuple[int,int],  artwork = "Ressources/Cartes/PlaceHolderDiscard.png"):
@@ -78,17 +91,18 @@ class DiscardPile:
         self.position = (position)
         self.cards = []
         self.size = 0
-        self.artwork = pygame.image.load(artwork)
+        self.artworkEmpty = pygame.image.load(artwork)
         self.scale = (35*5, 47*5)
 
     def add2Discard(self, card: Card):
-        self.cards.append(card)
-        self.size += 1
+        if card:
+            self.cards.append(card)
+            self.size += 1
 
     def rmFromDiscard(self, index: int):
-        if 0 < index < self.size:
-            self.size =-1
-            return self.card.pop(index)
+        if 0 <= index < self.size:
+            self.size -= 1
+            return self.cards.pop(index)
     
     def __str__(self):
         printer = "[ "
@@ -98,12 +112,12 @@ class DiscardPile:
         return printer
     
     def display(self):
-        self.artwork = pygame.transform.scale(self.artwork, self.scale)
+        if self.size == 0:
+            self.artwork = pygame.transform.scale(self.artworkEmpty, self.scale)
+        else:
+            self.artwork = pygame.transform.scale(self.cards[-1].artwork, self.scale)
         self.screen.blit(self.artwork, self.position)
-
-
-
-
+            
 class PlayerCard:
     def __init__(self, screen: pygame.surface.Surface, position: Tuple[int,int], hp = 20, mp = 1, ap = 1, artwork = "Ressources/Cartes/PlayerCard.png"):
         self.screen = screen
@@ -147,10 +161,6 @@ class PlayerCard:
             self.bigArtWork = pygame.transform.scale(self.artwork, (self.scale[0]*2.5, self.scale[1]*2.5))
             self.screen.blit(self.bigArtWork, (self.screenSize[0]/1.55, self.screenSize[0]/8))
 
-
-
-
-
 class Game:
     def __init__(self, pCard: PlayerCard, deck: Deck, discard: DiscardPile):
         self.screen = screen
@@ -172,12 +182,17 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.KEYDOWN:
+                discard.add2Discard(deck.rmFromTopDeck())
+            if event.type == pygame.KEYUP:
+                deck.add2Deck(discard.rmFromDiscard(discard.size-1))
+                deck.shuffle()
+                print(deck)
     
     def run(self):
         while self.running:
             self.display()
             self.event()
-
 
 if __name__ == "__main__":
     pygame.init()
@@ -188,7 +203,7 @@ if __name__ == "__main__":
 
     player = PlayerCard(screen, ((screenSize[0] - (35*5))/2, (screenSize[1] - (47*5))/2))
 
-    deck = Deck(screen, ((screenSize[0] - (35*5))*6/7, (screenSize[1] - (47*5))*6/7), [Card(2, "Fireball"), Card(3, "Scythe"), Card(2, "Heal"), Card(2, "tornado"), Card(3, "A WII FUCKING GUN"), Card(3, "brick"), Card(3, "Mana Crown")])
+    deck = Deck(screen, ((screenSize[0] - (35*5))*6/7, (screenSize[1] - (47*5))*6/7), [Card("Dmg2"), Card("HpUp3")])
     
     discard = DiscardPile(screen, ((screenSize[0] - (35*5))*26/35,(screenSize[1] - (47*5))*6/7))   
     
